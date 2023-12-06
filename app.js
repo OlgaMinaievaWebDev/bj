67d537f0cf1491d5b3c11189010deb3da0d65ea0
Olga <minaeva9@gmail.com>
2023-12-05 23:10:31 -0500

    modified

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-let cards = [];
+const startGameEl = document.getElementById("startGame");
+
+startGameEl.addEventListener('click', function () {
+ console.log('clicked');
+})
+
+function buildDeck() {
+  let values = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let types = ["♥️", "♠️", "♦️", "♣️"];
+ let deck = [];
+
+  for (let i = 0; i < types.length; i++) {
+   for (let j = 0; j < values.length; j++) {
+    deck.push(values[j] + " " + types[i]);
+    console.log(deck);
+    }
+  }
+}
+
